eee7c3e6c60fd5f6cc49a4b39c3b360a2efc2910
Nils Hartmann <nils@nilshartmann.net>
2017-05-05 12:34:06 +0200

    Add non-React playground for JSDoc

---
+++ code/sandbox/20-typecheck-only-ts23/src/playground.js
@@ -0,0 +1,64 @@
+/** @typedef {{
+  name: string,
+	age: number,
+	hobby?: string
+}} Person
+*/
+
+/**
+ * @type {Person}
+ */
+const p = {
+	name: 'Susi',
+	age: 43
+}
+
+/**
+ * @param {Person} person - ...
+ */
+function sayName(person) {
+	console.log(p.name);
+}
+
+sayName({
+	name: 'Dieter',
+	age: 22
+});
+
+/**
+ *@param {Readonly<Person>} person
+ */
+function dumpPerson(person) {
+	// Does not work as Person is read only:
+	// person.name = 'Someone';
+
+	console.log(person);
+}
+
+/**
+ * @param {Readonly<Person>} person
+ * @return {string} the hobby
+ */
+function getHobby(person) {
+	// why is this not an error?
+	return 33;
+}
+
+const theHobby = getHobby(p);
+
+/**
+ *@param {Person?} person - an optional person
+ *@return {string}
+ */
+function getTheName(person) {
+	// error: person might be null
+	// return person.name;
+
+	return person && person.name;
+}
+
+getTheName(p);
+getTheName(null);
+// is not a person:
+// getTheName('xxx');
+
