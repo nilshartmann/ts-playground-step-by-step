986d1552b1f7bbdbc297749cdc951a3af626483a
Nils Hartmann <nils@nilshartmann.net>
2017-06-18 11:10:45 +0200

    Comments

--- code/sandbox/00-plain-typescript/src/interfaces.ts
+++ code/sandbox/00-plain-typescript/src/interfaces.ts
@@ -16,23 +16,27 @@ function interfaces() {
 	interface Auditable { audit: () => void }
 	type Loggable = { log: string };
 
+	// Interface extends both a Type and an Interface
 	interface IAuditableAndLoggable extends Auditable, Loggable {
-
 	}
 
+	// Interface extends a Type
 	interface ConfigurableLoggable extends Loggable {
 		level: string;
 	}
 
+    // Type combines Interface and Type
 	type TAuditableAndLoggable = Auditable & Loggable;
 
-
+	// Class implements both an Interface and a Type
 	class Logger implements Loggable, Auditable {
 		log: string
 		audit() { }
 	}
 
-	class ExtendedLogger implements ConfigurableLoggable {
+
+	// Class implements a Type (that "consists" of Interface and Type itself)
+	class ExtendedLogger implements TAuditableAndLoggable {
 		log: string;
 		level: string;
 		audit() {}
